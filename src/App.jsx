31725b4854fc1d875947827fbe5ca03f8d55fc48
Rushikesh Bangar <rushikeshab17@gmail.com>
2024-01-21 01:12:13 +0530

    make skeleton of project and integrate redux

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { fetchDatafromApi } from './utils/api'
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice'
+
+function App() {
+
+  const dispatch = useDispatch()
+  useEffect(()=> {
+    apiTesting()
+  },[])
+  const apiTesting  = () => {
+    fetchDatafromApi("/movie/popular")
+    .then((res)=> {
+      dispatch(getApiConfiguration(res))
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+  return (
+    <div className='App'></div>
+  )
+}
+
+export default App
